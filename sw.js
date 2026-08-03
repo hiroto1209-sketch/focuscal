75bd9d23f65398ddf0263568c7813d1f1af63bcf
hiroto1209-sketch <hiroto1209@gmail.com>
2026-08-03 16:29:11 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'focuscal-v40';
+const CACHE = 'focuscal-v41';
 const ASSETS = ['./index.html','./guide.html','./privacy.html','./demo.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
