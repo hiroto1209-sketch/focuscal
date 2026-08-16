6de48bec5b1e0fcf65473ce396e8ae1836a25391
hiroto1209-sketch <hiroto1209@gmail.com>
2026-08-16 11:44:17 +0900

    Configure Firebase for FocusCal realtime sharing

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// FocusCal Firebase configuration
+window.FOCUSCAL_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAG3z8Uq40nGP1ofCcsFoMLcGZ4a4tsGhA",
+  authDomain: "focuscal-connects.firebaseapp.com",
+  projectId: "focuscal-connects",
+  storageBucket: "focuscal-connects.firebasestorage.app",
+  messagingSenderId: "570470010602",
+  appId: "1:570470010602:web:4142ec4b57990c81581193",
+  measurementId: "G-T82NHMXWSL"
+};
