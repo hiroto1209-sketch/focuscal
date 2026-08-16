01a0f4fd00e40b7629251c76e2974a858b75e09f
hiroto1209-sketch <hiroto1209@gmail.com>
2026-08-16 17:51:59 +0900

    Refresh FocusCal v56 assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='focuscal-v55-direct-boot';
-const CORE=['./index.html','./app.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='focuscal-v56-holidays-gestures';
+const CORE=['./index.html','./app.html','./manifest.json','./icon-192.png','./icon-512.png','./enhancements.js'];
 
 self.addEventListener('install',event=>{
   event.waitUntil((async()=>{
@@ -35,7 +35,7 @@ self.addEventListener('fetch',event=>{
     return;
   }
 
-  if(path.endsWith('/app.html')||path.endsWith('/peek.js')||path.endsWith('/firebase-config.js')||path.endsWith('/sw.js')){
+  if(path.endsWith('/app.html')||path.endsWith('/peek.js')||path.endsWith('/firebase-config.js')||path.endsWith('/enhancements.js')||path.endsWith('/sw.js')){
     event.respondWith(networkFirst(event.request));
     return;
   }
