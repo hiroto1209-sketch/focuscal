5f84ffba00a77a61fb43aa3ad7281c7f54b28af5
hiroto1209-sketch <hiroto1209@gmail.com>
2026-08-17 17:18:32 +0900

    Add FCM web push public configuration placeholder

---
+++ push-config.js
@@ -0,0 +1,4 @@
+// FocusCal Web Push public configuration.
+// Paste the Web Push certificate key pair PUBLIC key (VAPID key) from Firebase Console here.
+// This key is public by design and is safe to ship to the browser.
+window.FOCUSCAL_FCM_VAPID_KEY = window.FOCUSCAL_FCM_VAPID_KEY || '';
